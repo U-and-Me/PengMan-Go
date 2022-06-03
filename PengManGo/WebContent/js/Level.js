e0b94888449732f85aff71a360f4f40caf96de54
Kim Yu Na <yunasun0920@gmail.com>
2022-06-04 02:02:36 +0900

    통과한 스테이지만 열어두기

--- PengManGo/WebContent/js/Level.js
+++ PengManGo/WebContent/js/Level.js
@@ -1,8 +1,20 @@
 "use strict"
 
-function clickId(stage){
+function clickId(stage, concept){
+	
+	if(stage <= db_stage){
+	
+		var link = document.querySelector("#link");
+		var link_name = './Concept_' + concept + '.jsp';
+		link.href = link_name;
+		location.replace(link_name);
+    	window.open(link);
+    	
+	}
+
 	let cur_stage = stage;
 	
 	localStorage.setItem("stage", cur_stage);
 	//console.log(localStorage.getItem("stage"));
+	
 }
